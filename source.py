718d48e2bad27ea641886bda7927a392650c0048
Austin LaShure <austinglashure@gmail.com>
2021-03-02 22:43:13 -0500

    created basic pygame skeleton changed the venv the project was using so it had pygame

--- source.py
+++ source.py
@@ -1,4 +1,5 @@
 import random
+import pygame
 
 
 class Dice:
@@ -16,4 +17,30 @@ class Dice:
         return score
 
 
-dice = Dice()
+red = (250, 0, 0)
+green = (0, 250, 0)
+blue = (0, 0, 250)
+white = (250, 250, 250)
+black = (0, 0, 0)
+# the basic colors to illustrate things
+pygame.init()  # activates the pygame module
+dims = (800, 800)
+window = pygame.display.set_mode(dims)  # created window object with the dimensions
+pygame.display.set_caption("Ur EEE Tarded")  # self motivation
+clock = pygame.time.Clock()  # created clock object to set frame rate
+dice = Dice()  # created dice object to get dice rolls for the game
+
+
+on = True  # will be the boolean to flip and kill the program loop
+while on:  # basic pygame loop
+    window.fill(white)  # just so it isn't black
+    pygame.display.flip()  # refreshes the display rendering
+    for e in pygame.event.get():  # everything is triggered by events
+        if e.type == pygame.QUIT:  # pygame's built in QUIT will trigger if the X is clicked
+            on = False  # kill the program loop
+        elif e.type == pygame.KEYDOWN:  # to detect a key stroke
+            if e.key == pygame.K_ESCAPE:  # another way to kill the program
+                on = False  # kill it
+
+    clock.tick(60)  # sets the frame rate at 60 seconds
+
